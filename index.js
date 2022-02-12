75ca079df6720eec305be52e6daa15ead233d9b1
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-02-13 03:25:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Tobbias!Welcome\n'
+  const msg = 'Hello Bala!Welcome\n'
   res.end(msg);
 });
 
