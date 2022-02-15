9e8745ad6ac3bfa5d347a169c8b6e84fd6efc24d
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-02-15 16:52:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hotfix Done..!!\n'
+  const msg = 'Bugfix Done..!!\n'
   res.end(msg);
 });
 
