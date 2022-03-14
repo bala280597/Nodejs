2dd95cdc8dc81c6012fb377a93ed7e973eb4a35d
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-03-14 14:17:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Goedemiddag ! Fijne dag\n'
+  const msg = 'Goedemiddag ! Fijne dag :) \n'
   res.end(msg);
 });
 
